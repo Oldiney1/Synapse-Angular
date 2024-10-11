ec6578eb5e387d8404d6a3a3674b043a8b05c95d
oldiney <oldiney15@gmail.com>
2024-10-11 15:41:56 -0300

    projeto inicial

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
